d1cadf0e60c06e7c446616f6e566d09fe5a18aea
Matt Cooper <matt@matthewcooper.net>
2013-01-14 12:46:50 +0000

    Adding all files from local into repo.

--- README.md
+++ README.md
@@ -1,4 +1,39 @@
 cache-server
 ============
 
-A simple NodeJS cache server to sit in between local dev and remote environments
+A simple NodeJS cache server to sit in between local dev and remote environments.
+
+## Setup Instructions
+
+1. Clone repo to your server
+2. Edit example-config.json as required and save as config.json
+3. Run using `sudo node app.js` or setup an upstart conf file to run as a service in ubuntu, see cache-server.conf
+
+Once the cache server is running you can use it by proxying requests to your local dev sites into the cache server.
+
+For example in Apache you can use:
+
+<pre>
+ProxyPass           /    http://127.0.0.1:8888/
+ProxyPassReverse    /    http://127.0.0.1:8888/
+</pre>
+
+You might like to only do this with certain folders though, or at least not do it with JS and CSS folders so you can work locally on these files.
+
+## Config file
+
+You can use the placeholder `{server_name}` with your config file to automatically insert the host name for use with multiple websites like so:
+
+<pre>
+{
+    "cache_base": "/data/httpd/{server_name}/cache",
+    "remote_server": "http://int.{server_name}",
+    "server_port": 8888
+}
+</pre>
+
+The config file contains three settings:
+
+* `cache_base` - the folder you wish to cache files to.
+* `remote_server` - the URL of the remote server to load pages from
+* `server_port` - the port the
---
+++ app.js
@@ -0,0 +1,176 @@
+/*global require*/
+
+(function () {
+
+    "use strict";
+
+    // Load dependencies
+    var http = require('http');
+    var fs = require('fs');
+    var util = require('util');
+    var config = require('./config.json');
+
+    // Setup globals
+    var scope = this;
+    var cache_base = config.cache_base || '';
+    var remote_server = config.remote_server || '';
+    var server_port = config.server_port || 80;
+
+    function processUrl(url) {
+
+        if (url.indexOf('?') > 0) {
+            var urlParts = url.split('?');
+            var queryParts = urlParts[1].split('&');
+            var requestUri = urlParts[0];
+            var queryString = {};
+            for (var t = 0; t < queryParts.length; t++) {
+                var parts = queryParts[t].split('=');
+                queryString[parts[0]] = parts[1];
+            }
+            return { requestUri: requestUri, queryString: queryString };
+        } else {
+            return { requestUri: url, queryString: {} };
+        }
+
+    }
+
+    function checkSubdirectory(path) {
+
+        var exists = fs.existsSync(path);
+
+        if (exists === true) {
+            console.log('Checking for subdirectory: ' + path + ' [FOUND]');
+        } else {
+            console.log('Checking for subdirectory: ' + path + ' [NOT FOUND]. Creating...');
+            fs.mkdir(path, '0777');
+        }
+
+        return;
+
+    }
+
+    function retrieveFromCache(cachePath, callback) {
+
+        console.log('Retrieving from cache: ' + cachePath);
+
+        var htmlContent = '';
+
+        callback = callback || function () { return false; };
+
+        htmlContent += fs.readFileSync(cachePath);
+
+        callback.call(scope, htmlContent);
+
+    }
+
+    function retrieveFromRemote(requestPath, basePath, refParts, cachePath, callback) {
+
+        console.log('Retrieving from remote server: ' + requestPath);
+
+        var htmlContent = '';
+
+        callback = callback || function () { return false; };
+
+        var req = http.get(requestPath, function (res) {
+            res.setEncoding('utf8');
+
+            res.on('data', function (chunk) {
+                htmlContent += chunk;
+            });
+
+            res.on('end', function () {
+                //check if cache folder exists
+                checkSubdirectory(basePath);
+
+                //create directories for each layer of reference
+                var incrementPath = basePath;
+                for (var v = 1; v < (refParts.length - 1); v++) {
+                    incrementPath = incrementPath + '/' + refParts[v];
+                    checkSubdirectory(incrementPath);
+                }
+
+                if (typeof cachePath !== 'undefined') {
+                    console.log('Saving into cache: ' + cachePath);
+                    var fd = fs.openSync(cachePath, 'w+', '0777');
+                    fs.writeSync(fd, htmlContent, 0, htmlContent.length, 0);
+                    console.log('Writing file to: ' + cachePath);
+                    callback.call(this, true, htmlContent);
+                }
+            });
+        });
+
+        req.on('error', function (e) {
+            console.log('Problem with request: ' + e.message);
+            callback.call(this, false, htmlContent);
+        });
+
+    }
+
+    function init() {
+
+        var server = http.createServer(function (request, response) {
+
+            // Process URL and query params into suitable object
+            var urlObj = processUrl(request.url);
+            var requestUrl = request.url;
+            var refParts = [];
+
+            // Base path for vhost
+            var serverName = request.headers['x-forwarded-host'].replace(/[^\.]+\./, '');
+            var basePath = cache_base.replace('{server_name}', serverName);
+            var requestPath = remote_server.replace('{server_name}', serverName) + requestUrl;
+
+            console.log('Request recieved: ' + requestPath);
+            console.log('Request ref: ' + urlObj.queryString.ref);
+
+            if (typeof urlObj.queryString.ref === 'undefined') {
+                console.log('No REF parameter found - retrieving from remote: ' + requestPath);
+                retrieveFromRemote(requestPath, basePath, refParts);
+            } else {
+                // Ref cleansing
+                if (urlObj.queryString.ref.indexOf('/') === 0) {
+                    urlObj.queryString.ref = urlObj.queryString.ref.replace('/', '');
+                }
+                if (urlObj.queryString.ref.substr(-1, 1) === '/') {
+                    urlObj.queryString.ref = urlObj.queryString.ref.substr(0, (urlObj.queryString.ref.length - 1));
+                }
+                refParts = urlObj.queryString.ref.split('/');
+
+                var cachePath = basePath;
+                for (var v = 1; v < refParts.length; v++) {
+                    cachePath += '/' + refParts[v];
+                }
+
+                console.log('cachePath: ' + cachePath);
+
+                fs.exists(cachePath, function (exists) {
+                    if (exists) {
+                        console.log('File exists get from cache');
+                        retrieveFromCache(cachePath, function (htmlContent) {
+                            console.log('Writing out content length of ' + htmlContent.length);
+                            response.writeHead(200, { "Content-Type": "text/html" });
+                            response.end(htmlContent);
+                        });
+                    } else {
+                        console.log('File does NOT exist get from remote');
+                        retrieveFromRemote(requestPath, basePath, refParts, cachePath, function (result, htmlContent) {
+                            if (result !== false) {
+                                console.log('Writing out content length of ' + htmlContent.length);
+                                response.writeHead(200, { "Content-Type": "text/html" });
+                                response.end(htmlContent);
+                            }
+                        });
+                    }
+                });
+            }
+        });
+
+        // Listen on port defined in config, IP defaults to 127.0.0.1
+        server.listen(server_port);
+        console.log('Server running at http://127.0.0.1:' + server_port + '/');
+
+    }
+
+    init();
+
+})();
